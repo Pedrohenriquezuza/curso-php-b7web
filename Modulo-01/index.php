6f168f3d34c4a37855596283440cc5759f374655
Pedro Zuza <156633255+Pedrohenriquezuza@users.noreply.github.com>
2025-07-29 17:11:03 -0300

    Initial commit

---
+++ Modulo-01/index.php
@@ -0,0 +1,66 @@
+<?php
+// exemplo de String
+// $texto = 'Pera, uva, maça e salada mista';
+
+// //strlen() conta o numero de caracteres de uma string
+// $numeroCaracteres = strlen($texto);
+// $caracteresPalavraPera = strlen("pera");
+// $caracteresPalavraUva = strlen("uva");
+// $caracteresPalavraSalada = strlen("salada Mista");
+
+// //strpos() - Busca a posição de alguma palavra na string
+
+// $posicaoPalavraUva = strpos($texto, 'uva');
+// $posicaoPalavraPera = strpos($texto, 'Pera');
+// $posicaoPalavraSalada = strpos($texto, 'salada mista');
+
+// //substr() - Retorna uma Parte de uma string
+// $parteDaString = substr($texto, 0, 4);
+// //EXERCICIO PERA
+// $palavraPera = substr($texto, $posicaoPalavraPera, $caracteresPalavraPera);
+
+// //EXERCICIO UVA
+// $palavraUva = substr($texto, $posicaoPalavraUva, $caracteresPalavraUva);
+
+// //EXERCICIO SALADA
+// $palavraSalada = substr($texto, $posicaoPalavraSalada, $caracteresPalavraSalada);
+
+// echo "$palavraPera <br />";
+// echo "$palavraUva <br />";
+// echo $palavraSalada;
+
+
+//   // Exemplo de Integer
+//   $idade = 13;
+
+//   // Exemplo de float
+//   $altura = 1.75;
+
+//   // Exemplo de boolean
+//   $estaEstudando = false -->
+
+
+// //Exemplo de Integer
+// $idade = 33;
+
+// //Adição:
+// echo $idade+5;
+// echo '<br />';
+
+// //Subtração
+// echo $idade-$idade;
+// echo '<br />';
+
+// //Divisão
+// echo $idade/$idade;
+// echo '<br />';
+
+// //Multiplicação
+// echo $idade*$idade;
+// echo '<br />';
+
+
+// Exemplo de Float
+
+$altura = 1
+$altura = 1.75
