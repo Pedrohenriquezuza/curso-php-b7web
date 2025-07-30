c31dbb82698472e16e728e7998ed27f0c500c9f2
Pedro Zuza <156633255+Pedrohenriquezuza@users.noreply.github.com>
2025-07-30 14:56:05 -0300

    Introdução aos Arrays
    nessa aula aprendi um pouco sobre os arrays em php, como funciona as chaves, valores, como acessar um valor...

--- Modulo-01/index.php
+++ Modulo-01/index.php
@@ -1,114 +1,30 @@
 <?php
-// exemplo de String
-// $texto = 'Pera, uva, maça e salada mista';
 
-// //strlen() conta o numero de caracteres de uma string
-// $numeroCaracteres = strlen($texto);
-// $caracteresPalavraPera = strlen("pera");
-// $caracteresPalavraUva = strlen("uva");
-// $caracteresPalavraSalada = strlen("salada Mista");
+$nome = array("Pedro", "Maria", "Cleuza");
 
-// strpos() - Busca a posição de alguma palavra na string
+$numeros = [1,  2, 30];
 
-// $posicaoPalavraUva = strpos($texto, 'uva');
-// $posicaoPalavraPera = strpos($texto, 'Pera');
-// $posicaoPalavraSalada = strpos($texto, 'salada mista');
+$carros = [
+    "Alessandro" => 1,
+    "Maria" => 2,
+    "Pedro" => 30
+];
 
-// substr() - Retorna uma Parte de uma string
-// $parteDaString = substr($texto, 0, 4);
-// EXERCICIO PERA
-// $palavraPera = substr($texto, $posicaoPalavraPera, $caracteresPalavraPera);
+$informacoes = [
+    "Alessandro" => [
+        "carros" => 1,
+        "idade" => 30,
+        "altura" => 1.74,
+        "cidade" => "Rio De Janeiro"
+    ],
+    "Maria" => [
+        "carros" => 2,
+        "idade" => 25,
+        "altura" => 1.64,
+        "cidade" => "São Paulo"
+    ]
+];
 
-// //EXERCICIO UVA
-// $palavraUva = substr($texto, $posicaoPalavraUva, $caracteresPalavraUva);
-
-// //EXERCICIO SALADA
-// $palavraSalada = substr($texto, $posicaoPalavraSalada, $caracteresPalavraSalada);
-
-// echo "$palavraPera <br />";
-// echo "$palavraUva <br />";
-// echo $palavraSalada;
-
-
-//   // Exemplo de Integer
-//   $idade = 13;
-
-//   // Exemplo de float
-//   $altura = 1.75;
-
-//   // Exemplo de boolean
-//   $estaEstudando = false -->
-
-
-// //Exemplo de Integer
-// $idade = 33;
-
-// //Adição:
-// echo $idade+5;
-// echo '<br />';
-
-// //Subtração
-// echo $idade-$idade;
-// echo '<br />';
-
-// //Divisão
-// echo $idade/$idade;
-// echo '<br />';
-
-// //Multiplicação
-// echo $idade*$idade;
-// echo '<br />';
-
-
-// Exemplo de Float
-// NUMEROS DECIMAIS EM PHP
-// $altura = 1;
-// $altura = 1.0;
-
-// $num1 = 2.00;
-// $num2 = 2.50;
-
-// // echo 'Multiplicação: '.($num1*$num2);
-// // echo '<br />';
-// // echo 'Adição: '.($num1+$num2);
-// // echo '<br />';
-// // echo 'Subtração: '.($num1 - $num2);
-// // echo '<br />';
-// // echo 'Divisão: '.($num1/$num2);
-
-// // Função de Arredondamento
-// // round()
-// //$num3 = 2.40;
-// //echo 'Arredondamento com round: '.round($num3);
-
-// Função de Arredondamento para baixo --> floor()
-// //echo 'Arredondamento com floor: '.floor($num3);
-
-// Função que Arredonda para cima --> ceil() 
-// $num3 = 4.0001;
-// echo 'Arredondamento com ceil: '.ceil($num3);
-
-
-
-
-
-
-
-// Exemplo de Boolean
-$estaEstudando = true;
-
-var_dump($estaEstudando);
-$estaEstudando = !$estaEstudando;
-
-//Verdadeiro - true, 1
-//Falso - false, 0
-
-var_dump($estaEstudando);
-
-
-
-// if (!$estaEstudando) {
-//     echo 'Parabéns';
-// } else {
-//     echo 'Vá Estudar';
-// }
+echo '<pre>';
+var_dump($informacoes["Alessandro"]["carros"]);
+echo '<pre>';
